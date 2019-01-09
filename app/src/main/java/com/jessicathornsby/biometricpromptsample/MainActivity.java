40193d9f1292b7843474607997adc5386c6fdef1
JessicaThornsby <jessica.thornsby@gmail.com>
2019-01-09 16:48:01 +0000

    BiometricPrompt demo app

---
+++ app/src/main/java/com/jessicathornsby/biometricpromptsample/MainActivity.java
@@ -0,0 +1,65 @@
+package com.jessicathornsby.biometricpromptsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import android.os.Bundle;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import androidx.fragment.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Executor newExecutor = Executors.newSingleThreadExecutor();
+
+        FragmentActivity activity = this;
+
+        final BiometricPrompt myBiometricPrompt = new BiometricPrompt(activity, newExecutor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                if (errorCode == BiometricPrompt.ERROR_NEGATIVE_BUTTON) {
+                } else {
+                    Log.d(TAG, "An unrecoverable error occurred");
+                }
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Log.d(TAG, "Fingerprint recogonised successfully");
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Log.d(TAG, "Fingerprint not recognised");
+            }
+        });
+
+        final BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Title text goes here")
+                .setSubtitle("Subtitle goes here")
+                .setDescription("This is the description")
+                .setNegativeButtonText("Cancel")
+                .build();
+
+        findViewById(R.id.launchAuthentication).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myBiometricPrompt.authenticate(promptInfo);
+            }
+        });
+
+    }
+}
